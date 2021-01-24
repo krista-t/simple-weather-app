398dc7d93402cca1e27f2f0cd89ede4603bb99f5
krista-t <krista.tomicic@gmail.com>
2021-01-24 11:51:44 +0100

    js dynamic data

--- main.js
+++ main.js
@@ -12,14 +12,37 @@ function setQuery(e){
 }
 
 function getResults(query){
-  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${query}&appid=3fd396f291239318cc87dde08c90882e`)
-  .then(weather => {
-    return weather.json();
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&appid=${apikey}`)
+  .then(data => {
+    return data.json();
     // console.log(weather)
    })
     .then(displayResults);
 }
 
-function displayResults(weather){
-  console.log(weather)
+function displayResults(data){
+  console.log(data)
+  // City and country
+  document.querySelector("h4").innerHTML= `${data.name}, ${data.sys.country}`;
+ 
+  // Temperature
+  let temperature =document.querySelector("h5")
+  temperature.innerHTML= Math.round(data.main.temp).toString() + "&deg;C";
+
+  //Description
+  document.querySelector("h6").innerHTML = data.weather[0].main;
+
+  //Icon 
+  let icon = data.weather[0].icon;
+  // let iconUrl = "http://openweathermap.org/img/wn/" + icon + ".png";
+  let iconUrl = "http://openweathermap.org/img/wn/" +`${icon}` + ".png";
+  document.querySelector("img").src = iconUrl;
+
+  //Feels like
+  document.querySelector(".feel").innerHTML = "Feels Like: <br>" + Math.round(data.main.feels_like) + "&deg;C";
+
+
+  
+  
+  
 }
